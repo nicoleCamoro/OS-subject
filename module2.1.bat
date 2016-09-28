47cc81f1557847d524e07069c6a64d4f557d9d3a
Franchette Camoro <nicolecamoro@gmail.com>
2016-09-28 10:35:12 +0800

    update module 2.1 FINAL

--- module2.1.bat
+++ module2.1.bat
@@ -15,16 +15,13 @@ set /p drive=Type the letter of the drive you want to use:
 echo.
 echo.
 dir %drive%: >nul 2>nul
-if not %errorlevel%==1 (
-  goto DISPLAY
-)
-if %errorlevel%==1 (
-  goto ROOTchoice
-)
+if not %errorlevel%==1 (cls goto DISPLAY)
+if %errorlevel%==1 goto ROOTchoice
+
 
 :DISPLAY
 cls
-echo Displaying files...
+echo Displaying Root files...
 PING -n 2 127.0.0.1>nul
 echo.
 cd %drive%:\
@@ -36,13 +33,11 @@ goto ChooseFile
 echo.
 echo Choose a name in the list
 set /p folder=
+if [%folder%]==[] goto eDISPLAY
 dir %folder% >nul 2>nul
-if not %errorlevel%==1 (
-  goto DISPLAYfolder
-)
-if %errorlevel%==1 (
-  goto eDISPLAY
-)
+if %errorlevel%==1 goto eDISPLAY
+if not %errorlevel%==1 goto DISPLAYfolder
+
 
 :eDISPLAY
 echo Cannot find folder
@@ -51,7 +46,7 @@ pause
 
 :DISPLAYfolder
 cls
-echo Displaying files...
+echo Displaying Parent files...
 PING -n 2 127.0.0.1>nul
 dir %folder% /b /o:n /ad-h
 cd %folder%
@@ -62,17 +57,16 @@ goto ChooseFile2
 echo.
 echo Choose a name in the list
 set /p folder2=
+if [%folder2%]==[] goto eDISPLAY2
 dir %folder2% >nul 2>nul
-if not %errorlevel%==1 (
-  goto DISPLAYfolder2
-)
-if %errorlevel%==1 (
-  goto eDISPLAY2
-)
+if %errorlevel%==1 goto eDISPLAY2
+if not %errorlevel%==1 goto DISPLAYfolder2
+
 
 :DISPLAYfolder2
 cls
-echo Displaying files...
+echo Displaying child files...
+echo.
 PING -n 2 127.0.0.1>nul
 dir %folder2% /b /o:n /ad-h
 cd %folder2%
@@ -82,12 +76,15 @@ echo Choose next action:
 echo [1] Go to main menu
 echo [2] Log out
 choice /c 12 /n 
-rem redirect somewhere
+if %errorlevel%==1 goto ROOT
+if %errorlevel%==2 goto LOGOUT
 pause
 
 :eDISPLAY2
 echo We couldn't find the folder specified
-echo Try again
-pause
-cls
 goto ChooseFile2
+
+:LOGOUT
+echo Goodbye!
+PING -n 2 127.0.0.1>nul
+exit
